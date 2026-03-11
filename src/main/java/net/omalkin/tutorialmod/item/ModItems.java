017c93744554868b75a7b62c40aa5fd95cf3c923
Oliver Malkin <oliver.malkin@gmail.com>
2026-03-11 19:40:53 +0000

    Custom tooltips added

--- src/main/java/net/omalkin/tutorialmod/item/ModItems.java
+++ src/main/java/net/omalkin/tutorialmod/item/ModItems.java
@@ -1,12 +1,17 @@
 package net.omalkin.tutorialmod.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.omalkin.tutorialmod.TutorialMod;
 import net.omalkin.tutorialmod.item.custom.ChiselItem;
 import net.omalkin.tutorialmod.item.custom.FuelItem;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TutorialMod.MODID);
@@ -16,7 +21,17 @@ public class ModItems {
     public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
             () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> RADISH = ITEMS.register("radish",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)){
+                /*
+                    This is an anonymous class as it's changing the default Item class.
+                    Saves making a whole new class just to add one thing
+                 */
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.tutorialmod.radish"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredItem<Item> STARLIGHT_ASHES = ITEMS.register("starlight_ashes",
             () -> new Item(new Item.Properties()));
 
