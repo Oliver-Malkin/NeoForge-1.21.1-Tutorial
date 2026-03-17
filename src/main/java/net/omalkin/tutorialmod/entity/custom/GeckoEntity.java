5e6082cd617168e68afa86c41634a83dad18c38a
Oliver Malkin <oliver.malkin@gmail.com>
2026-03-17 22:48:07 +0000

    Boss bar

--- src/main/java/net/omalkin/tutorialmod/entity/custom/GeckoEntity.java
+++ src/main/java/net/omalkin/tutorialmod/entity/custom/GeckoEntity.java
@@ -2,13 +2,16 @@ package net.omalkin.tutorialmod.entity.custom;
 
 import net.minecraft.Util;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.BossEvent;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
@@ -27,8 +30,10 @@ import org.jetbrains.annotations.Nullable;
 public class GeckoEntity extends Animal {
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
-    // EntityDAtaAccessors are automatically synced between server and client
+    // EntityDataAccessors are automatically synced between server and client
     private static final EntityDataAccessor<Integer> VARIANT = SynchedEntityData.defineId(GeckoEntity.class, EntityDataSerializers.INT);
+
+    private final ServerBossEvent bossEvent = new ServerBossEvent(Component.literal("Our Mighty Gecko"), BossEvent.BossBarColor.GREEN, BossEvent.BossBarOverlay.NOTCHED_10);
 
     public GeckoEntity(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
@@ -90,7 +95,6 @@ public class GeckoEntity extends Animal {
     }
 
     // Variant stuffs
-
     @Override
     protected void defineSynchedData(SynchedEntityData.Builder builder) {
         super.defineSynchedData(builder);
@@ -144,5 +148,24 @@ public class GeckoEntity extends Animal {
     @Override
     protected @Nullable SoundEvent getDeathSound() {
         return SoundEvents.SHULKER_DEATH;
+    }
+
+    // Boss bar stuff
+    @Override
+    public void startSeenByPlayer(ServerPlayer serverPlayer) {
+        super.startSeenByPlayer(serverPlayer);
+        this.bossEvent.addPlayer(serverPlayer);
+    }
+
+    @Override
+    public void stopSeenByPlayer(ServerPlayer serverPlayer) {
+        super.stopSeenByPlayer(serverPlayer);
+        this.bossEvent.removePlayer(serverPlayer);
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+        this.bossEvent.setProgress(this.getHealth() / this.getMaxHealth());
     }
 }
